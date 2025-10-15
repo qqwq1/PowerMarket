b1e628b238cfc2ee6dc2560f1c73abb18c2413eb
ZyrT12 <Zyryanova554@gmail.com>
2025-10-15 14:38:59 +0500

    adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,107 @@
-plugins {
-	java
-	id("org.springframework.boot") version "3.5.6"
-	id("io.spring.dependency-management") version "1.1.7"
-}
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+        plugins {
+            application
+            jacoco
+            checkstyle
+            id("org.springframework.boot") version "3.5.4"
+            id("io.spring.dependency-management") version "1.1.7"
+            id("org.sonarqube") version "6.2.0.5505"
+            id("io.sentry.jvm.gradle") version "4.4.1"
+            id("io.freefair.lombok") version "8.4"
+        }
 
 group = "com.powermarket"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+
+    runtimeOnly("com.h2database:h2:2.2.224")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("at.favre.lib:bcrypt:0.10.2")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    implementation("org.flywaydb:flyway-core:10.17.3")
+    implementation("org.flywaydb:flyway-database-postgresql:10.17.3")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
+    testImplementation("org.assertj:assertj-core:3.26.0")
+    testImplementation("com.h2database:h2:2.2.224")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+configurations.all {
+    exclude(group = "org.eclipse.jetty")
+}
+
+application {
+    mainClass.set("hexlet.code.AppApplication")
+}
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    systemProperty("spring.profiles.active", "test")
+    finalizedBy(tasks.jacocoTestReport)
+    testLogging {
+        events = setOf(
+            TestLogEvent.PASSED,
+            TestLogEvent.FAILED,
+            TestLogEvent.SKIPPED
+        )
+        exceptionFormat = TestExceptionFormat.FULL
+        showExceptions = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.required = true
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "zyrt12")
+        property("sonar.projectKey", "ZyrT12_java-project-99")
+        property("sonar.sources", "src/main/java")
+        property("sonar.tests", "src/test/java")
+        property("sonar.java.binaries", "build/classes/java/main")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property ("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
