b43cfc9ebc9f97fb79d0cfb831725a2bff274ff9
ZyrT12 <Zyryanova554@gmail.com>
2025-10-15 20:19:33 +0500

    fix jacocoTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -28,4 +29,14 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
