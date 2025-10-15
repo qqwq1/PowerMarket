10d2f93c4ce6b76a5172d1f3b79c5549959d9b5a
ZyrT12 <Zyryanova554@gmail.com>
2025-10-15 15:30:13 +0500

    fix(ci): run Gradle in app/; clean gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,19 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-        plugins {
-            application
-            jacoco
-            checkstyle
-            id("org.springframework.boot") version "3.5.4"
-            id("io.spring.dependency-management") version "1.1.7"
-            id("org.sonarqube") version "6.2.0.5505"
-            id("io.sentry.jvm.gradle") version "4.4.1"
-            id("io.freefair.lombok") version "8.4"
-        }
+plugins {
+    jacoco
+    checkstyle
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "5.1.0.4882"
+    id("io.sentry.jvm.gradle") version "4.4.1"
+    id("io.freefair.lombok") version "8.4"
+}
 
 group = "com.powermarket"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
+description = "PowerMarket backend"
 
 java {
     toolchain {
@@ -36,11 +35,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.security:spring-security-oauth2-jose")
-
-    runtimeOnly("com.h2database:h2:2.2.224")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    implementation("org.mindrot:jbcrypt:0.4")
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("at.favre.lib:bcrypt:0.10.2")
 
@@ -50,6 +46,7 @@ dependencies {
     implementation("org.flywaydb:flyway-database-postgresql:10.17.3")
 
     runtimeOnly("org.postgresql:postgresql:42.7.3")
+    runtimeOnly("com.h2database:h2:2.2.224")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
@@ -62,9 +59,6 @@ configurations.all {
     exclude(group = "org.eclipse.jetty")
 }
 
-application {
-    mainClass.set("hexlet.code.AppApplication")
-}
 jacoco {
     toolVersion = "0.8.12"
 }
@@ -90,18 +84,5 @@ tasks.jacocoTestReport {
     reports {
         xml.required = true
         html.required = true
-    }
-}
-
-sonar {
-    properties {
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "zyrt12")
-        property("sonar.projectKey", "ZyrT12_java-project-99")
-        property("sonar.sources", "src/main/java")
-        property("sonar.tests", "src/test/java")
-        property("sonar.java.binaries", "build/classes/java/main")
-        property("sonar.junit.reportPaths", "build/test-results/test")
-        property ("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
