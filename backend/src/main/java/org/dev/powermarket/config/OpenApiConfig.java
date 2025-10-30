8a2f64d80591878d2c0afe4ea449c5d7fd64a022
ZyrT12 <Zyryanova554@gmail.com>
2025-10-30 13:46:51 +0500

    fix security config and jwt

---
+++ backend/src/main/java/org/dev/powermarket/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package org.dev.powermarket.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Powermarket API", version = "0.0.1"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig { }
